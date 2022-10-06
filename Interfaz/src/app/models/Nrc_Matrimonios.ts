3008ae6cb0dfbee4381261089c1c79265f79afd5
Christopher <cris12corona@gmail.com>
2022-10-06 09:06:31 -0500

    matrimonios
    correcion de la palabra matrimonios

--- Interfaz/src/app/models/Nrc_Matrimonios.ts
+++ Interfaz/src/app/models/Nrc_Matrimonios.ts
@@ -1,4 +1,4 @@
-export interface Nrc_Matrimomios{
+export interface Nrc_Matrimonios{
     numeroacta?:number;
     anioregistro?:number;
     tipodocumento?:number;
@@ -40,13 +40,13 @@ export interface Nrc_Matrimomios{
    p1_municipioregistro?:number | null;
    p1_oficialia?:number | null;
    p1_entidadregistro?:number | null;
-   p1_actabis?:number | null;
-   p1_curp?:number | null;
-   p1_cadena?:number | null;
+   p1_actabis?:string | null;
+   p1_curp?:string | null;
+   p1_cadena?:string | null;
    p2_nombres?:string;
    p2_primerapellido?:string;
    p2_segundoapellido?:string;
-   p2_fechanacimiento?:number | null;
+   p2_fechanacimiento?:Date| null;
 
 
       p2_sexo?: string | null;
@@ -56,13 +56,11 @@ export interface Nrc_Matrimomios{
       p2_entidadnacimiento?: number | null;
       p2_municipionacimiento?: number | null;
       p2_localidadnacimiento?:  string | null;
-      p2_numeroacta?: string | null;
+      p2_numeroacta?: number | null;
       p2_anioregistro?: number | null;
       p2_tipodocumento?: number | null;
       p2_entidadregistro?: number | null;
-//59 empiezas con este P2_MUNICIPIOREGISTRO sipi
-//entonces empiezo desde la 60 en adelante jajaja ok deja la busco 
-//dani
+
 
      p2_municioioregistro?:number | null;
      p2_oficialia?:number | null;
@@ -76,7 +74,7 @@ export interface Nrc_Matrimomios{
      p1_pa_anioregistro?:number | null;
      p1_pa_tipodocumento?:number | null;
 
-//chris
+
       p1_pa_entidadregistro?: number | null;
       p1_pa_municipioregistro?: number | null;
       p1_pa_oficilia?: number | null;
@@ -89,19 +87,19 @@ export interface Nrc_Matrimomios{
       p1_ma_segundoapellido?: string | null;
 
       
-//dani
+
       p1_ma_numeroacta?: number | null;
       p1_ma_anioregistro?: number | null;
-      p1_ma_tipodocumento?:string | null;
+      p1_ma_tipodocumento?:number | null;
       p1_ma_entidadregistro?:number | null;
-      p1_ma_municipioregistrado?:string | null;
+      p1_ma_municipioregistrado?:number | null;
       p1_ma_oficialia?:number | null;
       p1_ma_actabis?:string | null;
       p1_ma_curp?:string | null;
-      p1_ma_nacionalidad?:string | null;
-      p1_ma_cadena?:number | null;
+      p1_ma_nacionalidad?:number | null;
+      p1_ma_cadena?:string | null;
       
-//chris
+
       p2_pa_nombres?: string | null;
       p2_pa_primerapellido?: string | null;
       p2_pa_segundoapellido?: string | null;
@@ -112,20 +110,20 @@ export interface Nrc_Matrimomios{
       p2_pa_municipioregistro?: number | null;
       p2_pa_oficialia?: number | null;
       p2_pa_actabis?: string | null;
-      //dani
+     
 
-      p2_pa_curp?:number;
+      p2_pa_curp?:string | null;
       p2_pa_nacionalidad?:string | null;
-      p2_pa_cadena?:number;
-      p2_ma_nombres?:string;
-      p2_ma_primerapellido?:string;
-      p2_ma_segundoapellido?:string;
+      p2_pa_cadena?:string | null;
+      p2_ma_nombres?:string | null;
+      p2_ma_primerapellido?:string | null;
+      p2_ma_segundoapellido?:string | null;
       p2_ma_numeroacta?:number | null;
-      p2_ma_anioregistro?:Date | null;
-      p2_ma_tipodocumento?:string | null;
+      p2_ma_anioregistro?:number | null;
+      p2_ma_tipodocumento?:number | null;
       p2_mentidadregistro?:number | null;
      
-//chris
+
       p2_ma_municipioregistro?: number | null;
       p2_ma_oficialia?: number | null;
       p2_ma_actabis?: string | null;
@@ -137,7 +135,7 @@ export interface Nrc_Matrimomios{
       ot_errororigen?: string | null;
       ot_firma?: string| null;
       
-//dani
+
       ot_sello?:string| null;
       p1_pa_sexo?:string;
       p1_ma_sexo?:string;
@@ -145,7 +143,7 @@ export interface Nrc_Matrimomios{
       p2_ma_sexo?:string;
       tipocadena?:number | null;
       im_archivo?:string | null;
-      co_tipo?:number | null;
+      co_tipo?:string | null;
       co_fechaoriginal?:Date | null;
       co_transcripcion?:string| null;
       co_soporte?:string| null;
