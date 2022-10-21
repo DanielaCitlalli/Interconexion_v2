d849852d6d2caeb9d99937e898a9ef7e829f2236
DanielaBocanegra <danielabocanegra2019@gmail.com>
2022-10-21 13:35:11 -0500

    Configuración de Proxy

--- Interfaz/src/environments/environment.prod.ts
+++ Interfaz/src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 
 export const environment = {
   production: true,
+  //Para liberar a producción, usar 10.215.1.14
+  // apiUrl_inter: 'http://10.215.1.14:80/',
   apiUrl_inter: 'https://localhost:44321/',
   apiUrl: 'http://10.254.65.232:81/'
 };
