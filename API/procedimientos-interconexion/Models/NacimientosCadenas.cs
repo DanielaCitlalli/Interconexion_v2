084a2c177a4ba1cbe7e069047aec0b58c12ede6f
Osdgonzalez <glezmiramontesoscar@gmail.com>
2022-11-17 15:59:46 -0600

    Optimizacion de busqueda de cadenas en Nacimientos
    Ademas se cambio el archivo de log y se creo un nuevo modelo tanto en la API como en la interfaz

---
+++ API/procedimientos-interconexion/Models/NacimientosCadenas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace procedimientos_interconexion.Models
+{
+    public class NacimientosCadenas
+    {
+        public string Cadena { get; set; }
+        public string PePrimerapellido { get; set; }
+        public string PeSegundoapellido { get; set; }
+        public string PeNombres { get; set; }
+        public string PeCurp { get; set; }
+        public string OtErrororigen { get; set; }
+    }
+}
