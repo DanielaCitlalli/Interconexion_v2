328b8f28df2b15056d7090989892955d3bd4e2a4
Christopher <cris12corona@gmail.com>
2022-12-14 15:20:10 -0600

    checbox formulario

--- API/procedimientos-interconexion/Controllers/NrcMatrimoniosController.cs
+++ API/procedimientos-interconexion/Controllers/NrcMatrimoniosController.cs
@@ -47,6 +47,7 @@ namespace procedimientos_interconexion.Controllers
             return nrcMatrimonios;
         }
 
+
         // PUT: api/NrcMatrimonios/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
